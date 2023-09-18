7d751234fa1764bb50e415070b74074aa9a37d7f
stempz101 <kms111001@gmail.com>
2023-09-18 21:35:41 +0300

    first commit

---
+++ src/test/java/com/stempz/todo/todolist/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stempz.todo.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
